8d08d30b424ac0d90d5a34fb3d187c3b398855b0
OscarLS <oca040370@gmail.com>
2026-01-12 10:29:40 -0500

    Update deployment workflow and fix markdown link handling; change link in HeroSection

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://graduni.github.io',
-  baseUrl: '/',
+  baseUrl: '/graduni/',
 
   organizationName: 'graduni',
   projectName: 'graduni',
